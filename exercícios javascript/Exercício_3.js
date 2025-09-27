50630fd8dfb401e22f997302cca3ea10b98828c7
RyanF3l1p3 <ryanfelipess003@gmail.com>
2025-09-26 23:36:56 -0300

    Delete exercícios javascript directory

--- exercícios javascript/Exercício_3.js
+++
@@ -1,9 +0,0 @@
-let nome = "Patrick";
-let sobrenome = "Rocha";
-let anoNascimento = 2000;
-
-let nomeCompleto = nome + " " + sobrenome;
-
-console.log(`Olá meu nome é ${nomeCompleto} e nasci em ${anoNascimento}.`);
-
-//Eu estou usando no Node para executar os códigos javascript
